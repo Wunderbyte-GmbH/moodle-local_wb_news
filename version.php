771bb306dce5084727bd20f4224671a3c7fbbeb6
winnidat <thomas.winkler@wunderbyte.at>
2024-07-01 14:19:52 +0200

    templates

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_news';
 $plugin->release = '1.0.5';
-$plugin->version   = 2024070108;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024070109;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;    // Requires this Moodle version.
 $plugin->maturity = MATURITY_ALPHA;
