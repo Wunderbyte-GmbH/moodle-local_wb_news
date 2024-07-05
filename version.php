aa29c7331d949c2cd4141f469f86b9f9cdfeb8e1
Georg Maißer <georg.maisser@wunderbyte.at>
2024-07-05 10:49:21 +0200

    Version 1.0.7 (2024070500)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_news';
-$plugin->release = '1.0.6';
-$plugin->version   = 2024070300;       // The current module version (Date: YYYYMMDDXX).
+$plugin->release = '1.0.7';
+$plugin->version   = 2024070500;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;    // Requires this Moodle version.
 $plugin->maturity = MATURITY_ALPHA;
