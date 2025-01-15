cc436f2428b5b3a1385162ab5d48f8094ec41e76
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-01-15 12:25:37 +0100

    Version 1.1.28 (2025011500)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_news';
-$plugin->release = '1.1.27';
-$plugin->version   = 2025011000;       // The current module version (Date: YYYYMMDDXX).
+$plugin->release = '1.1.28';
+$plugin->version   = 2025011500;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;    // Requires this Moodle version.
 $plugin->maturity = MATURITY_ALPHA;
