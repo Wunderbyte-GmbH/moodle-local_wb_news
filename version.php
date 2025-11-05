83fa1e508e608be7e2a3cc26876930b2cc15ad7b
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-11-05 10:21:24 +0100

    Version 1.1.30 (2025110500)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_news';
 $plugin->supported = [404, 405];
-$plugin->release = '1.1.29';
-$plugin->version   = 2025051900;       // The current module version (Date: YYYYMMDDXX).
+$plugin->release = '1.1.30';
+$plugin->version   = 2025110500;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;    // Requires this Moodle version.
 $plugin->maturity = MATURITY_ALPHA;
